042de1b3a21e7bc9b58dc275dd606ffd19c9e9ec
Chris Bendel <chrissbendel@gmail.com>
2026-03-26 10:22:57 -0400

    lint

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "offline" => "pwa#offline", as: :pwa_offline
+  get "manifest" => "rails/pwa#manifest", :as => :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", :as => :pwa_service_worker
+  get "offline" => "pwa#offline", :as => :pwa_offline
 
   resource :session, only: [:new, :create, :destroy] do
     get :verify, on: :collection
