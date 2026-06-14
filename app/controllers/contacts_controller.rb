800c72054485a173479fa5b6cbe1566ce66fc2ee
Chris Bendel <chrissbendel@gmail.com>
2026-06-14 13:59:57 -0400

    Email spam (#185)
    * Dont report in dev
    
    * Rate limit and protect contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,7 @@
 class ContactsController < ApplicationController
+  rate_limit to: 2, within: 1.minute, only: :create,
+    with: -> { redirect_to new_contact_path, alert: "Too many requests. Please try again in a moment." }
+
   def new
   end
 
